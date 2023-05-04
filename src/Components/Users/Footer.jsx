1024f54622d93a7fd3da1a697add16d41f29c77e
Abner Estevez <nertrabajo2@gmail.com>
2023-05-04 11:00:39 -0400

    Borrando datos de consola

--- src/Components/Users/Footer.jsx
+++ src/Components/Users/Footer.jsx
@@ -6,23 +6,14 @@ import { useSelector } from "react-redux";
 
 var regex = /(\d+)/g;
 
-// let arr = ['Lunes 29 en la mañana', 'Martes 2 en la tarde', 'Jueves 18 en la mañana', 'martes 30 en la tarde', 'Lunes 8 en la mañana'];
-// let num = arr[1].match(regex);
-// let newArr = arr.sort((a, b) => {
-//   return a.match(regex) - b.match(regex);
-// })
-// console.log(newArr);
-
 function Footer() {
   const start = useSelector((state) => state.inicio);
-  // const diaSelect = "day" + start.diaSelect.toString();
   const [name, setName] = useState();
 
   useEffect(() => {
     const tasksRef = ref(dataB, start.monthCambiante);
     onValue(tasksRef, (snapshot) => {
       const data = snapshot.val()[start.name].name;
-      console.log(data);
       setName({
         name: data.sort((a, b) => {
           return a.match(regex) - b.match(regex);
